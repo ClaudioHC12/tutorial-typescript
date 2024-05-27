27af65f7dd836c345f26523ed2d67e195057bd11
Claudio HC <claudiohc1299@gmail.com>
2024-05-26 22:45:51 -0600

    Desarrollo 1ra parte curso

--- index.ts
+++ index.ts
@@ -1,22 +1,71 @@
 // tsc file.ts => node file.js   =   ts-node file.ts
 
-//Tipos de datos
-let var1: number = 1;
-let var2: string = 'a';
-let var3: boolean = true;
+function tiposDatos() {
+    //Tipos de datos
+    let var1: number = 1;
+    let var2: string = 'a';
+    let var3: boolean = true;
 
-let persona: { nombre: string, edad: number, direccion: { num: number, calle: string } } = {
-    nombre: 'claudio',
-    edad: 24,
-    direccion: {
-        num: 399,
-        calle: 'nicobar'
+    let persona: { nombre: string, edad: number, direccion: { num: number, calle: string } } = {
+        nombre: 'claudio',
+        edad: 24,
+        direccion: {
+            num: 399,
+            calle: 'nicobar'
+        }
+    };
+    console.log(persona);
+}
+
+function arreglos() {
+    //Arrays
+    let arrayLetras: string[] = ['a', 'bb', 'ccc'];
+    let arrayLongitud: number[] = arrayLetras.map((x) => x.length);
+    let total = 0;
+    arrayLongitud.forEach(x => {
+        total += x;
+    });
+    let valorDinamico: any = 1;
+    valorDinamico = '2';
+
+    console.log(arrayLongitud);
+}
+
+function tuplasEnums() {
+    //Tuplas
+    let automovil: [string, number, string] = ['BMW', 2024, 'X6'];
+    type vehiculo = [string, number, string];
+    let moto: vehiculo = ['Pular', 2024, 'x600'];
+
+    //Enums
+    enum rol { estudiante, desarrollador, programador };
+    console.log(rol['0']);
+
+    //Literal
+    let vehiculo1: 'vehiculo';
+}
+
+function ImplementacionFunciones() {
+    //Funciones 
+    function sumar(num1: number, num2: number): number {
+        return num1 + num2;
     }
-};
-//console.log(persona);
+    let sumar1 = (num1: number, num2: number): number => num1 + num2;
+    function imprimir(): void {
+        console.log(sumar1(1, 2));
+    }
 
+    //retorno Never
+    const trowError = (message: string) => {
+        throw new Error(message);
+    }
 
-
-
+    //Callback, pasar funcion como parametro
+    usoCallback('claudio', imprimir);
+    function usoCallback(usuario: string, funcionParametro: Function) {
+        console.log(usuario + ' ha ejecutado: ' + funcionParametro.name + '()');
+        funcionParametro();
+    }
+}
 
 
---
+++ interfaces.ts
@@ -0,0 +1,33 @@
+enum rol { doctor, enefermero, cirujano };
+interface Persona {
+    name: string,
+    email: string,
+    role: rol
+};
+type Empleado = {
+    name: string,
+    email: string,
+    role: rol
+};
+interface salario {
+    total: number,
+    sueldoDiario(): string
+};
+const medico = {
+    name: 'example',
+    email: 'example@g.com',
+    role: rol.doctor,
+    total: 2500,
+    sueldoDiario(): string { return 'salario diario: ' + this.total }
+};
+
+const imprimirPersonal = (objeto: Persona) => {
+    console.log(objeto);
+};
+imprimirPersonal(medico);
+
+const imprimirPersonalSalario = (objeto: salario) => {
+    console.log(objeto.sueldoDiario())
+};
+imprimirPersonalSalario(medico);
+
